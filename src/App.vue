620ec4881d1879a3aa1dac21b754f9c2dc4849c2
Uthmaan <samsodienuthmaan@gmail.com>
2023-07-07 16:47:45 +0200

    updates

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,6 @@ export default{
 
 </script>
 
-<style>
+<style src="@/assets/css/style.css">
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,46 @@
-<template><div class=" home">
-
-  <div class="container text-center">
-    <div class="row">
-      <div class="col">
-        <img class="photo animate__animated animate__fadeInLeftBig" src="https://i.postimg.cc/vTv25jC8/IMG-6957-sqaure.jpg" alt="">
-      </div>
-      <div class="col" id="homeDetails">
+<template>
+  <div class="home">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+          <img
+            class="photo animate__animated animate__fadeInLeftBig"
+            src="https://i.postimg.cc/vTv25jC8/IMG-6957-sqaure.jpg"
+            alt=""
+          />
+        </div>
+        <div class="col" id="homeDetails">
           <!-- <div class="row text animate__animated animate__fadeInLeftBig">
           </div > -->
-          <h1 class="animate__animated animate__fadeInRightBig">Hello, my name is Uthmaan.</h1>
-          <h5 class="animate__animated animate__fadeInRightBig 0.1s">An aspiring front-end developer.</h5>
-          <router-link to="/contact"> <button class=" btnproject animate__animated animate__fadeInRightBig">Lets connect</button></router-link>
-       
+          <h1 class="animate__animated animate__fadeInRightBig">
+            Hello, my name is Uthmaan.
+          </h1>
+          <h5 class="animate__animated animate__fadeInRightBig 0.1s">
+            An aspiring front-end developer.
+          </h5>
+          <router-link to="/contact">
+            <button
+              class="btnproject animate__animated animate__fadeInRightBig"
+            >
+              Lets connect
+            </button></router-link
+          >
+        </div>
       </div>
-
     </div>
   </div>
-</div>
-  
 </template>
 
 <script>
-
-export default {
-}
+export default {};
 </script>
 
 <style scoped>
 h1 {
   margin-bottom: 0;
+}
+
+.home {
 }
 
 #homeDetails {
@@ -37,17 +49,16 @@ h1 {
   justify-content: center;
 }
 
-@media only screen and (max-width :800px) {
-  .photo{
-width:18rem;
-top:0%;
-left:0%;
-transform: translate(1%, 40%);
-padding: 1rem;
+@media only screen and (max-width: 800px) {
+  .photo {
+    width: 18rem;
+    top: 0%;
+    left: 0%;
+    transform: translate(1%, 40%);
+    padding: 1rem;
   }
-  .headskill{ 
-  padding-top: 1rem;
+  .headskill {
+    padding-top: 1rem;
   }
- }
-
+}
 </style>
