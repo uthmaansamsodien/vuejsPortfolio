5cc2c90d3854fdc5b214ab0d42595acdae897521
Uthmaan <samsodienuthmaan@gmail.com>
2023-07-07 14:01:41 +0200

    updates

--- src/main.js
+++ src/main.js
@@ -9,4 +9,5 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import './assets/css/style.css'
 import 'animate.css';
 
+
 createApp(App).use(store).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,15 @@
   <div class="container text-center">
     <div class="row">
       <div class="col">
-        <img class="photo animate__animated animate__fadeInLeftBig col-md-6" src="https://i.postimg.cc/vTv25jC8/IMG-6957-sqaure.jpg" alt="">
+        <img class="photo animate__animated animate__fadeInLeftBig" src="https://i.postimg.cc/vTv25jC8/IMG-6957-sqaure.jpg" alt="">
       </div>
       <div class="col">
           <div class="row text animate__animated animate__fadeInLeftBig">
           </div >
           <h1 class="animate__animated animate__fadeInRightBig">Hello, my name is Uthmaan.</h1>
-          <h5 class="animate__animated animate__fadeInRightBig 0.1s">An aspiring full-stack developer.</h5>
-        
+          <h5 class="animate__animated animate__fadeInRightBig 0.1s">An aspiring front-end developer.</h5>
+          <router-link to="/contact"> <button class=" btnproject animate__animated animate__fadeInRightBig">Lets connect</button></router-link>
+       
       </div>
     </div>
   </div>
@@ -28,5 +29,18 @@ export default {
 h1 {
   margin-bottom: 0;
 }
+
+@media only screen and (max-width :800px) {
+  .photo{
+padding:1rem;
+width:20rem;
+top:0%;
+left:0%;
+transform: translate(1%, 40%);
+  }
+  .headskill{ 
+  padding-top: 1rem;
+  }
+ }
 
 </style>
